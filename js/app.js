f17be228ab8322d5d4b68e67f71fbdf601b4085f
Joshua Daniel <jrdaniel@ncsu.edu>
2016-08-16 04:14:16 -0400

    portfolio changes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
-$(document).ready(function(){
-   // Activate the side menu
-   $(".button-collapse").sideNav();
-  });
+// $(document).ready(function(){
+//
+//    // Activate the side menu
+//
+  //  $(".button-collapse").sideNav();
+//   });
+
+  (function($){
+    $(function(){
+
+      $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrain_width: false, // Does not change width of dropdown to that of the activator
+            hover: true, // Activate on hover
+            gutter: 0, // Spacing from edge
+            belowOrigin: false, // Displays dropdown below the button
+            alignment: 'left' // Displays dropdown with edge aligned to the left of button
+          }
+        );
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
